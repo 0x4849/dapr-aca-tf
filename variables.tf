d13fa40b42fbc166cc26ae1f65cf6ab76d286c75
0x4849 <bpharris@ualberta.ca>
2025-12-24 10:09:36 -0500

    Remove variable defaults and add main.tfvars

--- variables.tf
+++ variables.tf
@@ -1,69 +1,55 @@
 variable "location" {
   type    = string
-  default = "canadacentral"
 }
 
 variable "rg_name" {
   type    = string
-  default = "aca-dapr-demo-rg"
 }
 
 variable "env_name" {
   type    = string
-  default = "aca-env-demo"
 }
 
 variable "publisher_app_name" {
   type    = string
-  default = "publisher"
 }
 
 variable "worker_app_name" {
   type    = string
-  default = "worker"
 }
 
 variable "publisher_image" {
   type    = string
-  default = "docker.io/youruser/publisher:latest"
 }
 
 variable "worker_image" {
   type    = string
-  default = "docker.io/youruser/worker:latest"
 }
 
 variable "publisher_port" {
   type    = number
-  default = 8080
 }
 
 variable "worker_port" {
   type    = number
-  default = 5690
 }
 
 variable "sb_namespace_name" {
   type    = string
-  default = "sb-aca-dapr-demo2025"
 }
 
 variable "sb_queue_name" {
   type    = string
-  default = "orders"
 }
 
 variable "worker_min_replicas" {
   type    = number
-  default = 0
 }
 
 variable "worker_max_replicas" {
   type    = number
-  default = 3
 }
 
 variable "keda_message_threshold" {
   type    = number
-  default = 5
 }
